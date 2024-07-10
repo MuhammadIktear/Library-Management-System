30deedb1ebe92143f352b63d8885916a30ff48f9
Muhammad Iktear <iktear500@gmail.com>
2024-07-10 20:05:49 +0600

    update

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -105,13 +105,8 @@ USE_I18N = True
 USE_TZ = True
 
 import os
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'#Location of static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 MEDIA_URL = '/media/'
 
