a9c164f5404d0efcf24e33e9e4cc8be44e307d10
Muhammad Iktear <iktear500@gmail.com>
2024-07-10 17:53:19 +0600

    database added

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -64,16 +65,24 @@ WSGI_APPLICATION = 'library_management_system.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+# Database documentation https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
-}
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://lms_l4f1_user:ZQ0KnyqsID4CkCjbXvVApMOTUj6Qj3mP@dpg-cq77bf08fa8c73869ltg-a.oregon-postgres.render.com/lms_l4f1',
+    )
+}    
 
 AUTH_PASSWORD_VALIDATORS = [
     {
